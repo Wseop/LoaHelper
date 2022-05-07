ef641d331bc35dd73940277a2657cfd01b599ee3
wooseop <crazyz2000@naver.com>
2022-05-07 15:34:26 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import Navigation from './components/Navigation.js';
 import ContentRaid from './components/ContentRaid.js';
 import Profile from './components/Profile.js';
 import Exchange from './components/Exchange.js';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
@@ -14,7 +13,7 @@ function App() {
       <div className="App">
           <Navigation />
 
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.REACT_APP_URL_BASE}>
           <Switch>
             <Route exact path="/" component={Profile} />
             <Route path='/exchange' component={Exchange} />
